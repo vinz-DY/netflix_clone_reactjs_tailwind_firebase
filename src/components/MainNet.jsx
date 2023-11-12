aaa66a4c700257cd56eb505789c2de23b8a53153
vinz-DY <vincentducamp259@hotmail.com>
2023-11-12 01:46:21 +0100

    bacground main ok et gradient balck to right ok

--- src/components/MainNet.jsx
+++ src/components/MainNet.jsx
@@ -1,20 +1,30 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
-import requests from '../Requests'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import requests from "../Requests";
 
 const MainNet = () => {
-    const [movies, setMovies] = useState([])
+  const [movies, setMovies] = useState([]);
 
-    useEffect(()=> {
-        axios.get(requests.requestsPopular).then((response) => {
-          setMovies(response.data);
-        });
-    }, [])
-    console.log(movies)
+  const movie = movies[Math.floor(Math.random() * movies.length)];
+
+  useEffect(() => {
+    axios.get(requests.requestsPopular).then((response) => {
+      setMovies(response.data.results);
+    });
+  }, []);
 
   return (
-    <div>MainNet</div>
-  )
-}
+    <section className="w-full h-[550px] text-white">
+      <div className="w-full h-full">
+        <div className="absolute w-[500px] h-[550px] bg-gradient-to-r from-black"></div>
+        <img
+          className="h-full w-full object-cover"
+          src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`}
+          alt={movie?.title}
+        />
+      </div>
+    </section>
+  );
+};
 
-export default MainNet
+export default MainNet;
