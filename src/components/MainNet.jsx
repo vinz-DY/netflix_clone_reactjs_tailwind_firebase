3cf2ff817e7574fdaf6698221214bbdbc8062cbe
vinz-DY <vincentducamp259@hotmail.com>
2023-11-12 11:22:10 +0100

    ajout d une condition englobante la div qui contient le h1 pour eviter le titre precedent visible bug entre 2

--- src/components/MainNet.jsx
+++ src/components/MainNet.jsx
@@ -32,17 +32,19 @@ const MainNet = () => {
             alt={movie.title}
           />
         )}
-        <div className="absolute w-full top-[20%] p-4 md:p-8">
-          <h1 className="text-3xl md:text-5xl" >{ movie?.title}</h1>
-          <div>
-            <button className="border bg-gray-300 text-black border-gray-300 py-2 px-5">
-              Play
-            </button>
-            <button className="border text-white border-gray-300 py-2 px-5 ml-4">
-              Watch Later
-            </button>
+        {imageLoaded && movie && (
+          <div className="absolute w-full top-[20%] p-4 md:p-8">
+            <h1 className="text-3xl md:text-5xl">{movie?.title}</h1>
+            <div className="my-4">
+              <button className="border bg-gray-300 text-black border-gray-300 py-2 px-5">
+                Play
+              </button>
+              <button className="border text-white border-gray-300 py-2 px-5 ml-4">
+                Watch Later
+              </button>
+            </div>
           </div>
-        </div>
+        )}
       </div>
     </section>
   );
