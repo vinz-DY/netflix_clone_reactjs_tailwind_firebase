32d28351432f2eedc8a500c3e912cb5672526aaf
vinz-DY <vincentducamp259@hotmail.com>
2023-11-12 11:33:02 +0100

    ajout averview et css tailwind mediaquery

--- src/components/MainNet.jsx
+++ src/components/MainNet.jsx
@@ -43,7 +43,8 @@ const MainNet = () => {
                 Watch Later
               </button>
             </div>
-            <p className="text-gray-400 text-sm" >Released: { movie?.release_date}</p>
+            <p className="text-gray-400 text-sm" >Released: {movie?.release_date}</p>
+            <p className="w-full md:max-w-[70%] lg:max-w-[50%]: xl:max-w-[35%] text-gray-200:" > {movie?.overview} </p>
           </div>
         )}
       </div>
