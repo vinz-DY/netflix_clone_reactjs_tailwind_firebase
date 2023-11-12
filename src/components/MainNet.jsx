3ecddca569e40cd5f4ee42af9dc90deb2742f13f
vinz-DY <vincentducamp259@hotmail.com>
2023-11-12 11:39:49 +0100

    ajout de fonction truncate avec if else pour les ... et composant MainNet terminé ok

--- src/components/MainNet.jsx
+++ src/components/MainNet.jsx
@@ -18,6 +18,14 @@ const MainNet = () => {
     setImageLoaded(true);
   };
 
+  const truncateString = (str, num) => {
+    if (str?.length > num) {
+      return str.slice(0, num) + '...'
+    } else {
+      return str;
+    }
+  }
+
   return (
     <section className="w-full h-[550px] text-white">
       <div className="w-full h-full">
@@ -44,7 +52,7 @@ const MainNet = () => {
               </button>
             </div>
             <p className="text-gray-400 text-sm" >Released: {movie?.release_date}</p>
-            <p className="w-full md:max-w-[70%] lg:max-w-[50%]: xl:max-w-[35%] text-gray-200:" > {movie?.overview} </p>
+            <p className="w-full md:max-w-[70%] lg:max-w-[50%]: xl:max-w-[35%] text-gray-200:" > {truncateString(movie?.overview, 150)} </p>
           </div>
         )}
       </div>
