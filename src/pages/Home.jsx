eef1156044f0d4725e599da5370713b8d2bad320
vinz-DY <vincentducamp259@hotmail.com>
2023-11-12 12:07:22 +0100

    composant Row affichage des noms de rows via des props title

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import MainNet from '../components/MainNet'
+import Row from '../components/Row'
+import requests from '../Requests'
 
 const Home = () => {
   return (
-      <>
-          <MainNet/>
+    <>
+      <MainNet />
+      <Row title="Up Coming" fetchURL={requests.requestUpcoming} />
+      <Row title="Popular" fetchURL={requests.requestsPopular} />
+      <Row title="Trending" fetchURL={requests.requestsTrending} />
+      <Row title="Top Rated" fetchURL={requests.requestsTopRated} />
+      <Row title="Horror" fetchURL={requests.requestsHorror} />
     </>
-  )
+  );
 }
 
 export default Home
