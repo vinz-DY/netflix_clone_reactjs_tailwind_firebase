be488b5fbf485fd11c570b611a1d6a067082b16c
vinz-DY <vincentducamp259@hotmail.com>
2023-11-12 15:57:55 +0100

    ajout des chevrons gauche droite via group

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import Movie from "./movie";
+import {MdChevronLeft, MdChevronRight} from 'react-icons/md'
 
 
 const Row = ({ title, fetchURL }) => {
@@ -16,13 +17,24 @@ const Row = ({ title, fetchURL }) => {
   return (
     <>
       <h2 className="text-white font-bold md:text-xl p-4">{title}</h2>
-      <div className="relative flex items-center">
-        <div id={"slider"}>
+      <div className="relative flex items-center group">
+        <MdChevronLeft
+          className="bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"
+          size={40}
+        />
+        <div
+          id={"slider"}
+          className="w-full h-full overflow-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative"
+        >
           {movies.map(
             (item, id) =>
               item.backdrop_path && <Movie key={item.id} item={item} />
           )}
         </div>
+        <MdChevronRight
+          className="bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"
+          size={40}
+        />
       </div>
     </>
   );
