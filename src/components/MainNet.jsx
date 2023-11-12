0d78a6980cf294431ff5346a74806d6b56df9f31
vinz-DY <vincentducamp259@hotmail.com>
2023-11-12 10:28:05 +0100

    ajout dune condition pour eviter les erreur au chargement l img

--- src/components/MainNet.jsx
+++ src/components/MainNet.jsx
@@ -4,6 +4,7 @@ import requests from "../Requests";
 
 const MainNet = () => {
   const [movies, setMovies] = useState([]);
+  const [imageLoaded, setImageLoaded] = useState(false);
 
   const movie = movies[Math.floor(Math.random() * movies.length)];
 
@@ -13,15 +14,24 @@ const MainNet = () => {
     });
   }, []);
 
+  const handleImageLoad = () => {
+    setImageLoaded(true);
+  };
+
   return (
     <section className="w-full h-[550px] text-white">
       <div className="w-full h-full">
         <div className="absolute w-[500px] h-[550px] bg-gradient-to-r from-black"></div>
-        <img
-          className="h-full w-full object-cover"
-          src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`}
-          alt={movie?.title}
-        />
+        {movie && (
+          <img
+            onLoad={handleImageLoad}
+            className={`h-full w-full object-cover ${
+              imageLoaded ? "" : "hidden"
+            }`}
+            src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+            alt={movie.title}
+          />
+        )}
       </div>
     </section>
   );
