5e3b054667056bf17bad3dd97a07eb533211900d
vinz-DY <vincentducamp259@hotmail.com>
2023-11-17 21:49:01 +0100

    ajout des link login signup and /

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import { AuthContextProvider } from "./context/AuthContext";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Account from "./pages/Account";
 
 
 export default function App() {
@@ -11,6 +14,10 @@ export default function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/account" element={<Account/>} />
+        
       </Routes>
       </AuthContextProvider>
     </>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <div className='flex items-center justify-between p-4 z-[100] w-full absolute'>
+      <Link to='/' >
       <h1 className="text-red-600 text-4xl font-bold cursor-pointer">
         NARNAR
       </h1>
+      </Link>
       <div>
+        <Link to='/login'>
         <button className="text-white pr-4">Sign In</button>
+        </Link>
+        <Link to='/signup'>
         <button className="text-white bg-red-600 px-6 py-2 rounded cursor-pointer">
           Sign Up
         </button>
+        </Link>
       </div>
     </div>
   );
